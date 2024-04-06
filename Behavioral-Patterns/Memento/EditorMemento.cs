b24baafc16ab8083b9d482cb26732542240a5876
Nico Filips <nico.filips@de.sii.group>
2024-04-06 21:31:05 +0200

    feat: add Memento

---
+++ Behavioral-Patterns/Memento/EditorMemento.cs
@@ -0,0 +1,16 @@
+﻿namespace Memento;
+
+public class EditorMemento
+{
+    private string _state;
+
+    public EditorMemento(string state)
+    {
+        _state = state;
+    }
+
+    public string GetState()
+    {
+        return _state;
+    }
+}
