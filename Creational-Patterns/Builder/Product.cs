216d5980901b121e32d7e23835a9344184c0f08b
Nico Filips <nico.filips@de.sii.group>
2024-04-17 14:07:35 +0200

    refactor(BuilderPattern): naming

--- Creational-Patterns/Builder/Product.cs
+++ Creational-Patterns/Builder/Product.cs
@@ -2,47 +2,41 @@
 
 public class Product
 {
-    // Fields representing parts of the complex object
-    public string PartA { get; private set; }
-    public string PartB { get; private set; }
-    public string PartC { get; private set; }
+    public string? PartA { get; private set; }
+    public string? PartB { get; private set; }
+    public string? PartC { get; private set; }
 
     public class Builder
     {
-        // Fields initialized with default values
-        private string partA = "DefaultA";
-        private string partB = "DefaultB";
-        private string partC = "DefaultC";
-
-        // Method to set partA and return the Builder for chaining
+        private string _partA = "DefaultA";
+        private string _partB = "DefaultB";
+        private string _partC = "DefaultC";
+        
         public Builder SetPartA(string partA)
         {
-            this.partA = partA;
+            _partA = partA;
             return this;
         }
-
-        // Method to set partB and return the Builder for chaining
+        
         public Builder SetPartB(string partB)
         {
-            this.partB = partB;
+            _partB = partB;
             return this;
         }
-
-        // Method to set partC and return the Builder for chaining
+        
         public Builder SetPartC(string partC)
         {
-            this.partC = partC;
+            _partC = partC;
             return this;
         }
-
-        // Method to construct the final Product object
+        
         public Product Build()
         {
             return new Product
             {
-                PartA = this.partA,
-                PartB = this.partB,
-                PartC = this.partC
+                PartA = _partA,
+                PartB = _partB,
+                PartC = _partC
             };
         }
     }
