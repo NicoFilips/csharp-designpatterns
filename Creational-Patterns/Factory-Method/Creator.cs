1beb60593de6eecb2a908bf36f24d4d6384c344a
Nico Filips <nico.filips@de.sii.group>
2024-04-17 14:09:14 +0200

    refactor(FactoryMethod): naming

--- Creational-Patterns/Factory-Method/Creator.cs
+++ Creational-Patterns/Factory-Method/Creator.cs
@@ -2,18 +2,13 @@
 
 namespace Factory_Method;
 
-// The 'Creator' abstract class
 public abstract class Creator
 {
-    // Factory Method is abstract and needs to be implemented by the subclass
     public abstract IProduct FactoryMethod();
-
-    // The Operation, which uses the Product. This Product will be created by the Factory Method.
+    
     public string SomeOperation()
     {
-        // Calls the Factory Method, to create a product-object.
         var product = FactoryMethod();
-        // uses the created Product.
         return $"Creator: The same creator's code has just worked with {product.Operation()}";
     }
 }
