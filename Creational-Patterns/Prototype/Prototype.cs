ded1badebe896d3fdf132dd387437370e9b4eb93
Nico Filips <nico.filips@de.sii.group>
2024-04-17 14:11:02 +0200

    refactor(prototype): exclude comments

--- Creational-Patterns/Prototype/Prototype.cs
+++ Creational-Patterns/Prototype/Prototype.cs
@@ -1,17 +1,13 @@
-using System;
-
 namespace Prototype;
 
-// The 'Prototype' abstract class
 public abstract class Prototype
 {
     public int Id { get; private set; }
 
-    public Prototype(int id)
+    protected Prototype(int id)
     {
-        this.Id = id;
+        Id = id;
     }
-
-    // The Clone method is declared abstract so it must be implemented in derived classes.
+    
     public abstract Prototype Clone();
 }
