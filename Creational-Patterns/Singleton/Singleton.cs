0c70aa2a2b3ccf45772427fb6469b6d4848848f8
Nico Filips <nico.filips@de.sii.group>
2024-04-17 14:12:59 +0200

    refactor(SingletonPattern): codestyle changes

--- Creational-Patterns/Singleton/Singleton.cs
+++ Creational-Patterns/Singleton/Singleton.cs
@@ -2,21 +2,12 @@
 
 public sealed class SingletonService
 {
-    // A private static variable that holds the single instance of the class.
-    private static readonly SingletonService instance = new SingletonService();
-
-    // Private constructor ensures that the class cannot be instantiated from outside.
-    private SingletonService()
-    {
-    }
-
-    // A public static property that provides access to the single instance.
-    public static SingletonService Instance
-    {
-        get { return instance; }
-    }
-
-    // An example method that can be called on the singleton instance.
+    private static readonly SingletonService _singletonService = new();
+    
+    private SingletonService(){}
+    
+    public static SingletonService Instance => _singletonService;
+    
     public void DoSomething()
     {
         Console.WriteLine("Singleton Pattern!");
